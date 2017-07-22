dd15441b123d82a45d847f268e1de7d46891e2d8
Alois Barreras <alois@vicino.io>
2017-07-22 16:35:05 -0400

    added route to return complaints

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,62 @@
 var express = require('express');
 var router = express.Router();
+const { Pool } = require('pg');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/complaints', (req, res, next) => {
+
+  const pool = new Pool();
+
+  pool.query('SELECT * FROM angel_hack.complaints', (err, result) => {
+    if (err) {
+      throw err
+    }
+
+    console.log('Found numer of rows: ' + result.rowCount);
+    res.set('Content-Type', 'application/json').send({
+      data: result.rows
+    });
+  })
+});
+
+/**
+ * initialReceiptDate
+ * caseContactMethod
+ * summary
+ * caseRegion
+ * caseCountry
+ * sector
+ * subSector
+ * category
+ * brand
+ * segment
+ * subBrand
+ * productFormDetail
+ * flavorScentDetail
+ * collection
+ * version
+ * consumerBenefit1
+ * consumerBenefit2
+ * consumerBenefit3
+ * consumerBenefit4
+ * productSize
+ * unitSize
+ * productionCode
+ * manufacturingPlant
+ * manufacturingDate
+ * manufacturingLine
+ * city
+ * stateProvince
+ * globalCommentPath
+ * L1commentCode
+ * L2commentCode
+ * L3commentCode
+ * commentCriticality
+ * storeOfPurchase
+ * complaintMetric
+ * 
+ */
+router.post('/customerInput', (req, res, next) => {
+
 });
 
 module.exports = router;
