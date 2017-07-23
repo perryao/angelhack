09e207ec65d2bea71c4a054cd533e4bd2c435e54
Alois Barreras <alois@vicino.io>
2017-07-22 21:58:34 -0400

    converting production code to nanoseconds

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const Influx = require('influx');
+const toNano = require('influx').toNanoDate;
 const { Pool } = require('pg');
 
 const influx = new Influx.InfluxDB({
@@ -19,17 +20,7 @@ const influx = new Influx.InfluxDB({
   ]
 });
 
-influx.getDatabaseNames()
-  .then(names => {
-    console.log(names);
-  })
-  .catch(err => {
-    console.log(err);
-    console.error(`Error creating Influx database!`);
-  })
-
 router.get('/complaints', (req, res, next) => {
-
   const pool = new Pool();
 
   pool.query('SELECT initial_receipt_date, case_contact_method, summary FROM angel_hack.complaints WHERE production_code is not null', (err, result) => {
@@ -45,8 +36,57 @@ router.get('/complaints', (req, res, next) => {
 });
 
 router.get('/timeseries', (req, res, next) => {
+  const productionCode = req.query.productionCode;
+  if (!productionCode) return res.send();
 
+  const nanoseconds = productionCodeToNanoseconds(productionCode);
+  const minus1hour = nanoMinus1Hour(nanoseconds);
+  const plus1hour = nanoPlus1Hour(nanoseconds);
+
+  res.send();
+
+  // influx.query('select a_kghr from  "plant_data"."autogen"."data" where time < now() - 1h limit 1000')
+  // influx.query('select b_kghr from  "plant_data"."autogen"."data" where time > 1493658000000 limit 1000;')
+  //   .then(result => {
+  //     res.set('Content-Type', 'application/json').send({ data: result });
+  //   })
+  //   .catch(err => res.send(err));
 });
+
+function productionCodeToNanoseconds(code) {
+  code = String(code);
+  const yearNumber = code.slice(0, 1);
+  const julianDate = code.slice(1, 4);
+  const plantCode = code.slice(4, 8);
+  const lineNumber = code.slice(8, 10);
+  const militaryTime = code.slice(10, 14);
+
+  const hours = militaryTime.slice(0, 2);
+  const minutes = militaryTime.slice(2);
+
+  const year = '201' + yearNumber;
+  const date = makeDate(year, julianDate, hours, minutes);
+  const milliseconds = date.getTime();
+  const nanoseconds = milliseconds * 1000000;
+
+  return nanoseconds;
+}
+
+function makeDate(year, day, hours, minutes) {
+  var date = new Date(year, 0); // initialize a date in `year-01-01`
+  date = new Date(date.setDate(day)); // add the number of days
+  date = new Date(date.setHours(hours));
+  date = new Date(date.setMinutes(minutes));
+  return date;
+}
+
+function nanoMinus1Hour(nano) {
+  return nano - (1 * 60 * 60 * 1000 * 1000000);
+}
+
+function nanoPlus1Hour(nano) {
+  return nano + (1 * 60 * 60 * 1000 * 1000000);
+}
 
 /**
  * initialReceiptDate
